5595cb66c8a482226433599c259815998a95afc8
Leonardo Ferreira <leo.oliveira.eng@outlook.com>
2020-12-14 19:19:08 -0300

    creating models

---
+++ Closest.Network.Location.API/Models/Address.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Closest.Network.Location.API.Models
+{
+    public class Address
+    {
+        #region Properties
+
+        public string Cep { get; private set; }
+
+        public string Street { get; private set; }
+
+        public string Number { get; private set; }
+
+        public string Complement { get; private set; }
+
+        public string City { get; private set; }
+
+        public string UF { get; private set; }
+
+        public double[] Location { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        [Obsolete("Created only for EF", true)]
+        public Address() { }
+
+        public Address(string cep, string street, string number, string complement, string city, string uF)
+        {
+            Cep = cep;
+            Street = street;
+            Number = number;
+            Complement = complement;
+            City = city;
+            UF = uF;
+        }
+
+        #endregion
+    }
+}
---
+++ Closest.Network.Location.API/Models/GasStation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Closest.Network.Location.API.Models
+{
+    public class GasStation
+    {
+        #region Properties
+
+        public string Id { get; private set; }
+
+        public DateTime CreatedAt { get; private set; } = DateTime.Now;
+
+        public DateTime LastUpdate { get; private set; } = DateTime.Now;
+
+        public DateTime? DeletedAt { get; private set; }
+
+        public bool Deleted => DeletedAt.HasValue;
+
+        public string ExternalId { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string PhoneNumber { get; private set; }
+
+        public string SiteUrl { get; private set; }
+
+        public Address Address { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        [Obsolete("Created only for EF", true)]
+        public GasStation() { }
+
+        public GasStation(string externalId, string name, string phoneNumber, string siteUrl, Address address)
+        {
+            ExternalId = externalId;
+            Name = name;
+            PhoneNumber = phoneNumber;
+            SiteUrl = siteUrl;
+            Address = address;
+        }
+
+        #endregion
+    }
+}
