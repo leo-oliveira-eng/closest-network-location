819291c66c8c51ad6627dbe2b6e5618d253373bd
Leonardo Ferreira <leo.oliveira.eng@outlook.com>
2020-12-14 18:21:50 -0300

    project structure

---
+++ Closest.Network.Location.API/Controllers/BaseController.cs
@@ -0,0 +1,51 @@
+﻿using Messages.Core;
+using Messages.Core.Enums;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Closest.Network.Location.API.Controllers
+{
+    public abstract class BaseController : ControllerBase
+    {
+        protected async Task<IActionResult> WithResponseAsync<TResponseMessage>(Func<Task<Response<TResponseMessage>>> func)
+        {
+            var response = await func.Invoke();
+
+            if (!response.HasError)
+                return Ok(response);
+
+            if (response.Messages.Any(message => message.Type == MessageType.BusinessError))
+                return BadRequest(response);
+
+            return StatusCode(500, response);
+        }
+
+        protected async Task<IActionResult> WithResponseAsync(Func<Task<Response>> func)
+        {
+            var response = await func.Invoke();
+
+            if (!response.HasError)
+                return Ok(response);
+
+            if (response.Messages.Any(message => message.Type == MessageType.BusinessError))
+                return BadRequest(response);
+
+            return StatusCode(500, response);
+        }
+
+        protected IActionResult WithResponse(Func<Response> func)
+        {
+            var response = func.Invoke();
+
+            if (!response.HasError)
+                return Ok(response);
+
+            if (response.Messages.Any(message => message.Type == MessageType.BusinessError))
+                return BadRequest(response);
+
+            return StatusCode(500, response);
+        }
+    }
+}
---
+++ Closest.Network.Location.API/Controllers/MeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Closest.Network.Location.API.Controllers
+{
+    [Route("api/[controller]"), ApiController]
+    public class MeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new { name = "Closest Network Location", version = "0.1" });
+    }
+}
